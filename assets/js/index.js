0a98bf22ae2ef59cf149cc1df92ff5f030c93397
ThesllaDev <theslladev@gmail.com>
2021-04-22 17:59:51 -0300

    ✨Feat [Home, TaskListCards]: drag and drop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// PROGRESS BAR
 $(function () {
     var $ppc = $('.progress__bar'),
         percent = parseInt($ppc.data('percent')),
@@ -8,9 +9,10 @@ $(function () {
     $('.bar__progress--fill').css('transform', 'rotate(' + deg + 'deg)');
 });
 
+// SIDE BAR
 const sideBar = document.querySelector('.lateral__menu');
 const btnOpenSideBar = document.querySelector('.btn__open__side__menu');
-btnOpenSideBar.style.display = "none"
+btnOpenSideBar.style.display = "none";
 
 function openSideBar() {
     sideBar.style.display = "block";
@@ -20,4 +22,31 @@ function openSideBar() {
 function closeSideBar() {
     sideBar.style.display = "none";
     btnOpenSideBar.style.display = "block";
+}
+
+// DRAG AND DROP CARD'S
+
+const cards = document.querySelectorAll('.cards');
+const taskLists = document.querySelectorAll('.list__cards');
+
+cards.forEach(card => {
+    card.addEventListener('dragstart', dragstart);
+    card.addEventListener('dragend', dragend);
+})
+
+function dragstart() {
+    this.classList.add('is-dragging');
+}
+
+function dragend() {
+    this.classList.remove('is-dragging');
+}
+
+taskLists.forEach(taskList => {
+    taskList.addEventListener('dragover', dragover);
+})
+
+function dragover() {
+    const cardBeingDragged = document.querySelector('.is-dragging');
+    this.insertAdjacentElement('afterbegin', cardBeingDragged);
 }
